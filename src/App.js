9840f0aa7cf16da722349745fbb2faed4ccbc7a3
franco-canedo <francanedo1@hotmail.com>
2020-12-09 15:55:32 -0600

    basic template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+function onlyOnce() {
+  console.log('run once');
+  return 4;
+}
 
 function App() {
+
+  const [state, setState] = useState({count: 4, theme: 'blue'});
+  const count = state.count
+  const theme = state.theme
+
+  function decrementCount() {
+    setState(prevState => {
+      return {...prevState, count: prevState.count - 1}
+    })
+  }
+
+  const incrementCount = () => {
+    setState(prevState => {
+      return {...prevState, count: prevState.count + 1}
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <button onClick={decrementCount}>-</button>
+  <span>{count}</span>
+  <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
+    </React.Fragment>
+  )
 }
 
 export default App;
