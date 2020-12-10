45c3eead91c9267b26dfafebd55019c3770ba93c
franco-canedo <francanedo1@hotmail.com>
2020-12-10 14:04:27 -0600

    colors added

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const Box = (props) => {
     return (
-        <div className='box'>
+        <div className='box' style={{backgroundColor: props.color}}>
             <h1>{props.number}</h1>
         </div>
     
--- src/Containers/Game.js
+++ src/Containers/Game.js
@@ -6,15 +6,50 @@ import Box from '../Components/Box';
 
 const Game = () => {
 
-    const [boxes, setState] = useState([
-      '1','2','3','4','5','6','7','8','9','10'
-    ]); 
+    const [boxes, setState] = useState(['1','2','3','4','5','6','7','8','9','10']); 
+    const [colors, setColor] = useState([
+        'rgb(153, 255, 153)', 
+        'rgb(153, 255, 255)', 
+        'rgb(255, 153, 153)', 
+        'rgb(153, 255, 153)', 
+        'rgb(153, 230, 255)', 
+        'rgb(204, 153, 255)'
+    ])
 
-    function createBoxes() {
-        for(let i = 0; i < 10; i++) {
-            console.log('boxes?')
-            return <h1>box</h1>
+    const createBoxes = (boxes) => {
+        let i = Math.floor(Math.random() * colors.length);
+        console.log(i);
+        shuffle(boxes);
+        return boxes.map(number => {
+            return <Box number={number} color={colors[i]}/>
+        })
+    }
+
+    const shuffle = (boxes) => {
+        let m = boxes.length, t, i;
+        while(m) {
+            i = Math.floor(Math.random() * m--);
+
+            t = boxes[m];
+            boxes[m] = boxes[i];
+            boxes[i] = t;
         }
+        return boxes;
+    }
+
+    const arrayLoop = (boxes) => {
+        let i = 0;
+        return boxes.map(number => {
+            
+            if (i < 7) {
+                i++;
+                return createBoxes(boxes);
+                
+            } else {
+                return;
+            }
+            
+        })
     }
 
 
@@ -22,12 +57,10 @@ const Game = () => {
         <div className='game'>
             <h1 className="gameTitle">Game</h1>
            <div className='gameSpace'>
-                {
-                      boxes.map(number => {
-                          return <Box number={number}/>
-                      })
-                    
-                }
+                
+        
+                
+                {arrayLoop(boxes)}
            </div>
         </div>
         
